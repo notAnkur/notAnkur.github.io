911a308228c30e433f81d50d44c456c0d102d217
Ankur Anant <ankuranant99@gmail.com>
2020-04-23 13:55:19 +0530

    Updates

---
+++ precache-manifest.9572c8cf3a4b5ce7860f43265fccc0c1.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd4555d82cf6d3cf834a3a55bb775a64",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b63351a1b3fdbae65a0d",
+    "url": "/static/css/2.cd1899df.chunk.css"
+  },
+  {
+    "revision": "f99b961b3c64921e4ad6",
+    "url": "/static/css/main.c3f75744.chunk.css"
+  },
+  {
+    "revision": "b63351a1b3fdbae65a0d",
+    "url": "/static/js/2.07d8d8c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.07d8d8c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f99b961b3c64921e4ad6",
+    "url": "/static/js/main.4a4221eb.chunk.js"
+  },
+  {
+    "revision": "4fba741fb7176f89c72d",
+    "url": "/static/js/runtime-main.ac97a943.js"
+  },
+  {
+    "revision": "c1c4f1b4c2afbc84e8e7c82a5df92a19",
+    "url": "/static/media/AR Shoe App.c1c4f1b4.png"
+  },
+  {
+    "revision": "63bf289860af56adbac9eb48c8a981d1",
+    "url": "/static/media/AccureMeetVR.63bf2898.png"
+  },
+  {
+    "revision": "48bd49c12e4e0c5cab16939307036df6",
+    "url": "/static/media/AdventOfCode.48bd49c1.png"
+  },
+  {
+    "revision": "3832394267c432b928590e3d676011ce",
+    "url": "/static/media/Background Gradient Generator.38323942.png"
+  },
+  {
+    "revision": "95f66701a60ceff6e1b3027617f9f730",
+    "url": "/static/media/Multiplayer FPS.95f66701.png"
+  },
+  {
+    "revision": "3c8c9b1766441788e205d60dd7e99fc1",
+    "url": "/static/media/Space Shooter.3c8c9b17.png"
+  },
+  {
+    "revision": "e56f20324d98733791bcd35f296fcfb5",
+    "url": "/static/media/Student Dashboard.e56f2032.png"
+  },
+  {
+    "revision": "e7eb3b6ab8117021bc135707491d1da0",
+    "url": "/static/media/android_studio.e7eb3b6a.png"
+  },
+  {
+    "revision": "5627cf5d7e0fdf2a6cf3fa4957eab9b1",
+    "url": "/static/media/css.5627cf5d.svg"
+  },
+  {
+    "revision": "533b7fc254db198440b2f9f62b6ed94c",
+    "url": "/static/media/mongodb.533b7fc2.png"
+  },
+  {
+    "revision": "b7402566665875ef369225467a8b4f53",
+    "url": "/static/media/profile.b7402566.png"
+  },
+  {
+    "revision": "d603c5d423df907f48d9132a8a3868e3",
+    "url": "/static/media/reactjs.d603c5d4.png"
+  }
+]);
